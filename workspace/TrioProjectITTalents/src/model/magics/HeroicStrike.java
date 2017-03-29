d960ba0807d9a7e906243701d58ada73ab90090c
Petko <petkogotsov53@gmail.com>
2017-03-29 21:21:01 +0300

    New buttons

---
+++ workspace/TrioProjectITTalents/src/model/magics/HeroicStrike.java
@@ -0,0 +1,12 @@
+package model.magics;
+
+import model.champions.Champion;
+
+public class HeroicStrike extends DamageSpells{
+
+	public HeroicStrike() {
+		super(MagicTypes.HEROICSTRIKE,29,17000);
+	}
+
+
+}
