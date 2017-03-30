f369123b8cedc161874719d460b670f2bb8ad1e3
Vladislav Docev <Vladislav Docev>
2017-03-30 15:18:27 +0300

    faf

---
+++ TrioProjectITTalents-master/workspace/TrioProjectITTalents/src/model/magics/KillingSpree.java
@@ -0,0 +1,16 @@
+package model.magics;
+
+import model.champions.Champion;
+
+public class KillingSpree extends Buffs{
+
+	public KillingSpree() {
+		super(MagicTypes.KILLINGSPREE,17,20000,30000);
+	}
+
+	@Override
+	public void affect(Champion target) {
+		target.increaseDamage(this.getDamage(),this.getDuration());
+	}
+
+}
